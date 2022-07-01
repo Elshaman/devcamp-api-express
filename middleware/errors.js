a4eef0125f1634ab0ef75b1235fb8ca2b2087e26
Your Name <you@example.com>
2022-07-01 11:10:14 -0500

    error validacion: datos de bootcamp suplicados

--- middleware/errors.js
+++ middleware/errors.js
@@ -6,12 +6,19 @@ const errorHandler  = (err , req , res , next) => {
     let error = {...err}
     error.message = err.message
 
-    console.log(err.stack.red);
+    console.log(err);
     
     //41 creamos el objeto error: el id se extrae en el parametrovalue
+    //caso de id erroneo
     if(err.name === 'CastError'){
         const message = `Bootcamp not found with id ${err.value}`
         error = new ErrorResponse(message,404)
+    }
+
+    //42 caso en que tenemos duplicados establecidos a partir del modelo
+    if(err.code === 11000){
+        const message  = 'Duplicate field value entered'
+        error = new ErrorResponse(message, 400)
     }
 
     res.status(error.statusCode || 500).json({
