cc936b5adafba599940e61a92d21fb1bfe3b7f45
Cristian Buitrago <cbo@misena.edu.co>
2022-07-13 09:24:45 -0500

    actualizado bootcamp con costo promedio de cursos

--- models/Course.js
+++ models/Course.js
@@ -57,7 +57,13 @@ CourseSchema.statics.getAverageCost = async function(bootcampId){
         }
     ])
 
-    console.log(obj)
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId,{
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (error) {
+        
+    }
 }
 
 
